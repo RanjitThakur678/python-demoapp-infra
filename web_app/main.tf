ce0d25d1ed732de69508690a8c8fff0d1223b472
Ranjit Thakur <75175061+RanjitThakur678@users.noreply.github.com>
2025-01-09 18:35:34 +0530

    Acr add (#2)
    * added workflow and webapp tf
    
    * making change
    
    * adding workflow,tfstate,webapp
    
    * Update workflow.yaml
    
    adding trigger
    
    * Update workflow.yaml
    
    test
    
    * added az
    
    * yes
    
    * test
    
    * test
    
    * Update workflow.yaml
    
    test
    
    * Update workflow.yaml
    
    test
    
    * test
    
    * test
    
    * acr addded
    
    * test
    
    * test
    
    * Update main.tf
    
    test
    
    * Update variables.tf
    
    test
    
    * Update workflow.yaml
    
    test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * Update main.tf
    
    main.tf
    
    * Update terraform.tfvars
    
    tfvars
    
    * Update variables.tf
    
    vars
    
    * Update workflow.yaml
    
    test
    
    * Update workflow.yaml
    
    added role
    
    * removed unwanted files

--- web_app/main.tf
+++ web_app/main.tf
@@ -1,33 +1,71 @@
+
+# Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
 
+# Service Plan
 resource "azurerm_service_plan" "plan" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux" # "Windows"
-  # sku_tier            = "Basic"
+  os_type             = "Linux"
   sku_name            = "B1"
 }
 
+# Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = var.container_registry_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+# Web App for Containers
 resource "azurerm_linux_web_app" "app" {
   name                = var.app_service_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.plan.id
 
-  site_config {
-
-    application_stack {
-      python_version =  "3.9" 
-    }
+  identity {
+    type = "SystemAssigned"
   }
+
+  site_config {
+    
+    application_stack {
+      python_version = "3.9"
+    }
+
+
+  }
+
+
 
   app_settings = {
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
-    "SCM_DO_BUILD_DURING_DEPLOYMENT"     = "true"
+    "DOCKER_ENABLE_CI"                   = "true"
+    "DOCKER_REGISTRY_SERVER_URL"          = azurerm_container_registry.acr.login_server
+    "DOCKER_REGISTRY_SERVER_USERNAME"     = azurerm_container_registry.acr.admin_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD"    = azurerm_container_registry.acr.admin_password
   }
+}
 
+# Role Assignment for ACR Pull
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = azurerm_linux_web_app.app.identity[0].principal_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+}
+
+# Outputs
+output "webapp_url" {
+  value = azurerm_linux_web_app.app.default_hostname
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
 }
